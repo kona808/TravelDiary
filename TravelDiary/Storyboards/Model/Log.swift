5e2ebab16d218fe5cb97b410f14687a4773c55c9
Jacob I  Perez <jacobperez91@gmail.com>
2022-09-22 04:47:14 -1000

    created model

---
+++ TravelDiary/Storyboards/Model/Log.swift
@@ -0,0 +1,25 @@
+//
+//  Log.swift
+//  TravelDiary
+//
+//  Created by Jacob Perez on 9/22/22.
+//
+
+import Foundation
+// Title, Address, Date, Body
+
+class Log {
+    
+    let title: String
+    let address: String
+    let logDate: Date
+    let body: String
+    
+    init(title: String, address: String, logDate: Date, body: String) {
+        
+        self.title = title
+        self.address = address
+        self.logDate = logDate
+        self.body = body
+    }
+}
